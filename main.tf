9bb0b4a989fab1170ccbcb8da0bf9ebde81cdee2
Movses <mmartirosyan69@gmail.com>
2025-09-01 17:01:21 +0400

    fixed issue with vpc

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ terraform {
     }
   }
 }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.0.1"
-}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
 
   name = "ecs-vpc"
   cidr = "10.0.0.0/16"
