99bbd43baa0e55e6ed8839068fe355172d23b757
Movses <mmartirosyan69@gmail.com>
2025-09-05 10:53:56 +0400

    initial infra deployment

--- ecs.tf
+++ ecs.tf
@@ -3,15 +3,16 @@ module "ecs" {
   version = "6.3.0"
 
   cluster_name = "my-ecs-cluster"
-  count = var.desired_count
 
   services = {
     simple-ecs-service = {
       subnet_ids = module.vpc.private_subnets
       launch_type = "FARGATE"
       security_group_ids = [aws_security_group.ecs_security_group.id]
+      desired_count = var.desired_count
       container_definitions = {
-
+        cpu                = 256
+        memory             = 512
       }
     }
   }
